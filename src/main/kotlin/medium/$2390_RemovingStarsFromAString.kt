53747142392762c638c414f95ae98e45ec5d3973
phil <jaeja.phil@gmail.com>
2023-11-19 13:05:53 +0900

    feat(): [LeetCode 75][Medium][2390] Removing Stars From A String

---
+++ src/main/kotlin/medium/$2390_RemovingStarsFromAString.kt
@@ -0,0 +1,69 @@
+package medium
+
+/**
+ * You are given a string s, which contains stars *.
+ *
+ * In one operation, you can:
+ *
+ * Choose a star in s.
+ * Remove the closest non-star character to its left, as well as remove the star itself.
+ * Return the string after all stars have been removed.
+ *
+ * Note:
+ *
+ * The input will be generated such that the operation is always possible.
+ * It can be shown that the resulting string will always be unique.
+ *
+ * Constraints:
+ * - 1 <= s.length <= 10^5
+ * - s consists of lowercase letters and stars *.
+ * - the operation above can be performed on s
+ *
+ * Example 1:
+ * Input: s = "leet**cod*e"
+ * Output: "lecoe"
+ * Explanation: Performing the removals from left to right:
+ * - The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+ * - The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+ * - The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+ * There are no more stars, so we return "lecoe".
+ *
+ * Example 2:
+ * Input: s = "erase*****"
+ * Output: ""
+ * Explanation: The entire string is removed, so we return an empty string.
+ *
+ */
+fun main() {
+    val res1 = `Removing Stars From A String`("leet**cod*e")
+    println(res1) // "lecoe"
+
+    val res2 = `Removing Stars From A String`("erase*****")
+    println(res2) // ""
+}
+
+fun `Removing Stars From A String`(s: String): String {
+    /**
+     * create a string builder to store the result as well as use it as
+     * a stack to remove the closest non-star character to its left.
+     */
+    val sb = StringBuilder()
+
+    /**
+     * Iterate through the string and remove the closest non-star character to its left.
+     */
+    s.forEach {
+        if (it == '*') {
+            if (sb.isNotEmpty()) {
+                sb.deleteAt(sb.length - 1)
+            }
+        } else {
+            sb.append(it)
+        }
+    }
+
+    /**
+     * return the result.
+     */
+    return sb.toString()
+}
