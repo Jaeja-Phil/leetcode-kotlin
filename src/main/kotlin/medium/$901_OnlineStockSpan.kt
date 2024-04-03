a99f19304752c0f4ed701a107ff84d9fb004963a
phil <jaeja.phil@gmail.com>
2024-04-03 16:18:22 +0900

    feat(): [LeetCode 75][Medium][901] Online Stock Span

---
+++ src/main/kotlin/medium/$901_OnlineStockSpan.kt
@@ -0,0 +1,63 @@
+package medium
+
+import java.util.Stack
+
+/**
+ * Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price
+ * for the current day.
+ *
+ * The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and
+ * going backward) for which the stock price was less than or equal to the price of that day.
+ * - For example, if the prices of the stock in the last four days is [7,2,1,2] and the price of the stock today is 2,
+ *   then the span of today is 4 because starting from today, the price of the stock was less than or equal 2 for 4
+ *   consecutive days.
+ * - Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then
+ *   the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3
+ *   consecutive days.
+ *
+ * Implement the StockSpanner class:
+ * - StockSpanner() Initializes the object of the class.
+ * - int next(int price) Returns the span of the stock's price given that today's price is price.
+ *
+ * constraints:
+ * - 1 <= price <= 10^5
+ * - at most 10^4 calls will be made to next.
+ *
+ * Example 1:
+ * Input
+ * ["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+ * [[], [100], [80], [60], [70], [60], [75], [85]]
+ * Output
+ * [null, 1, 1, 1, 2, 1, 4, 6]
+ * Explanation
+ * StockSpanner stockSpanner = new StockSpanner();
+ * stockSpanner.next(100); // return 1
+ * stockSpanner.next(80);  // return 1
+ * stockSpanner.next(60);  // return 1
+ * stockSpanner.next(70);  // return 2
+ * stockSpanner.next(60);  // return 1
+ * stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+ * stockSpanner.next(85);  // return 6
+ */
+fun main() {
+    class StockSpanner() {
+        private val stack: Stack<Pair<Int, Int>> = Stack()
+        fun next(price: Int): Int {
+            var span = 1
+            while (stack.isNotEmpty() && stack.peek().first <= price) {
+                span += stack.pop().second
+            }
+            stack.push(Pair(price, span))
+            return span
+        }
+    }
+
+    val stockSpanner = StockSpanner()
+    println(stockSpanner.next(100)) // return 1
+    println(stockSpanner.next(80))  // return 1
+    println(stockSpanner.next(60))  // return 1
+    println(stockSpanner.next(70))  // return 2
+    println(stockSpanner.next(60))  // return 1
+    println(stockSpanner.next(75))  // return 4
+    println(stockSpanner.next(85))  // return 6
+}
