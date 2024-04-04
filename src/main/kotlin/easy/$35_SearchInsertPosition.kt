4c534b65e2b3ca69c136a337c4a40fb366dc7fa5
phil <jaeja.phil@gmail.com>
2024-04-04 14:02:29 +0900

    feat(): [Binary Search][Easy][35] Search Insert Position

---
+++ src/main/kotlin/easy/$35_SearchInsertPosition.kt
@@ -0,0 +1,47 @@
+package easy
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return
+ * the index where it would be if it were inserted in order.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ * constraints:
+ * - 1 <= nums.length <= 10^4
+ * - -10^4 <= nums[i] <= 10^4
+ * - nums contains distinct values sorted in ascending order.
+ * - -10^4 <= target <= 10^4
+ *
+ * Example 1:
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ *
+ * Example 2:
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ *
+ * Example 3:
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ */
+fun main() {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.lastIndex
+        while (left < right) {
+            val mid = left + (right - left) /2
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] > target -> right = mid -1
+                else -> left = mid + 1
+            }
+        }
+
+        return if (nums[left] >= target) left else left + 1
+    }
+
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 5)) // 2
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 2)) // 1
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 7)) // 4
+    println(searchInsert(intArrayOf(1, 3, 5, 6), 0)) // 0
+}
