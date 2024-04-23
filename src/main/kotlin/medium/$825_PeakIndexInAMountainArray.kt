5877b4a2569e1abfa74d95c658d1a75fd34d4030
phil <jaeja.phil@gmail.com>
2024-04-23 21:30:50 +0900

    feat(): [Binary Search][Medium][825] Peak Index In a Mountain Array

---
+++ src/main/kotlin/medium/$825_PeakIndexInAMountainArray.kt
@@ -0,0 +1,55 @@
+package medium
+
+/**
+ * An array arr is a mountain if the following properties hold:
+ * - arr.length >= 3
+ * - There exists some i with 0 < i < arr.length - 1 such that:
+ *   - arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+ *   - arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+ *
+ * Given a mountain array arr, return the index i such that:
+ * arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+ *
+ * You must solve it in O(log(arr.length)) time complexity.
+ *
+ * Constraints:
+ * - 3 <= arr.length <= 10^4
+ * - 0 <= arr[i] <= 10^6
+ * - arr is guaranteed to be a mountain array.
+ *
+ * Example 1:
+ * Input: arr = [0,1,0]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: arr = [0,2,1,0]
+ * Output: 1
+ *
+ * Example 3:
+ * Input: arr = [0,10,5,2]
+ * Output: 1
+ */
+fun main() {
+    fun peakIndexInMountainArray(arr: IntArray): Int {
+        var left = 0
+        var right = arr.lastIndex
+        while (left < right) {
+            val mid = left + (right - left) / 2
+            val midNext = mid + 1
+            if (arr[mid] > arr[midNext]) {
+                // must be on the left side (inclusive), shift right window of search boundary to mid
+                right = mid
+            } else {
+                // must be on the right side, shift left window of search boundary after mid
+                left = midNext
+            }
+        }
+
+        return left
+    }
+
+    println(peakIndexInMountainArray(intArrayOf(0, 1, 0))) // 1
+    println(peakIndexInMountainArray(intArrayOf(0, 2, 1, 0))) // 1
+    println(peakIndexInMountainArray(intArrayOf(0, 10, 5, 2))) // 1
+    println(peakIndexInMountainArray(intArrayOf(0, 1, 2, 3, 4, 2, 1, 0)))
+}
