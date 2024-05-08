e2c22a932dafdb19ed09b64b9c1b80a1e866b185
phil <jaeja.phil@gmail.com>
2024-05-08 14:30:40 +0900

    feat(): [Top Interview 150][Easy][14] Longest Common Prefix

---
+++ src/main/kotlin/easy/$14_LongestCommonPrefix.kt
@@ -0,0 +1,47 @@
+package easy
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ *
+ * constraints:
+ * - 1 <= strs.length <= 200
+ * - 0 <= strs[i].length <= 200
+ * - strs[i] consists of only lower-case English letters.
+ *
+ * Example 1:
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ */
+fun main() {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        // find the min length string
+        val minLength = strs.minOf { it.length }
+
+        // iterate through the min length string
+        repeat(minLength) { i ->
+            // get the first character of the first string
+            val char = strs[0][i]
+
+            // check if the character is not the same in the rest of the strings
+            if (strs.any { it[i] != char }) {
+                return strs[0].take(i)
+            }
+        }
+
+        return strs[0].take(minLength)
+    }
+
+    val tests = listOf(
+        arrayOf("flower", "flow", "flight"), // "fl"
+        arrayOf("dog", "racecar", "car"), // ""
+    )
+
+    tests.forEach { test ->
+        println(longestCommonPrefix(test))
+    }
+}
