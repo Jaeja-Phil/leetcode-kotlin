8cd3681502f61b9cb127c4187dc8a621cd61815b
phil <jaeja.phil@gmail.com>
2025-02-21 18:04:43 +0900

    feat(): [Medium][416] Partition Equal Subset Sum

---
+++ src/main/kotlin/medium/$416_PartitionEqualSubsetSum.kt
@@ -0,0 +1,42 @@
+package medium
+
+/**
+ * Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the
+ * elements in both subsets is equal or false otherwise.
+ *
+ * Constraints:
+ * - 1 <= nums.length <= 200
+ * - 1 <= nums[i] <= 100
+ *
+ * Example 1:
+ * Input: nums = [1,5,11,5]
+ * Output: true
+ * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,5]
+ * Output: false
+ * Explanation: The array cannot be partitioned into equal sum subsets.
+ */
+fun main() {
+    fun canPartition(nums: IntArray): Boolean {
+        val sum = nums.sum()
+        if (sum % 2 != 0) return false
+
+        val target = sum / 2
+        val dp = BooleanArray(target + 1)
+        dp[0] = true
+
+        for (num in nums) {
+            for (i in target downTo num) {
+                dp[i] = dp[i] || dp[i - num]
+                if (dp[target]) return true
+            }
+        }
+
+        return dp[target]
+    }
+
+    println(canPartition(intArrayOf(1, 5, 11, 5))) // true
+    println(canPartition(intArrayOf(1, 2, 3, 5))) // false
+}
