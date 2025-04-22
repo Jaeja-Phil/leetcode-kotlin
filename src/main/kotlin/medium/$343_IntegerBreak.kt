d7271d19d44bcc1f778c653172b60b5f9c966931
phil <jaeja.phil@gmail.com>
2025-04-22 17:00:41 -0400

    feat(): [Medium][343] Integer Break
    - add intuition comment

--- src/main/kotlin/medium/$343_IntegerBreak.kt
+++ src/main/kotlin/medium/$343_IntegerBreak.kt
@@ -21,7 +21,27 @@ package medium
  */
 fun main() {
     fun integerBreak(n: Int): Int {
-        val dp = IntArray(n + 1) { 1 }
+        /**
+         * For any number "i", what's the best product we can get by breaking it into at least two positive integers?
+         * We can use dynamic programming to solve this problem.
+         * > dp[i] = max product we can get from integer i
+         *
+         * since the constraint is 2 <= n, we can start from 2
+         * for i == 2, we can only break it into 1 + 1, so dp[2] = 1 (which is initialized to 1)
+         *
+         * so, for i from 3 to n, we can break it into j + (i - j) for all j from 2 to i - 1
+         * We have two options:
+         * 1. don't break i - j any further, just multiply: j * (i - j)
+         * 2. do break i - j further, so multiply j * dp[i - j]
+         *
+         * So, we can take the maximum of these options:
+         * > dp[i] = maxOf(dp[i], j * (i - j), j * dp[i - j])
+         *
+         * try every valid j, and pick the best one
+         */
+        val dp = IntArray(n + 1)
+        dp[1] = 1 // base case, we can't break 1 into two positive integers
+        dp[2] = 1 // base case, we can only break 2 into 1 + 1, so the product is 1
 
         for (i in 3..n) {
             for (j in 2..<i) {
